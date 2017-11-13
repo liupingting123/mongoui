40245ca1b313b9368c8fee0a4d13c9db99f5142d
liupingting123 <1483705281@qq.com>
2017-11-13 18:04:54 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var ExtendedDefinePlugin = require('extended-define-webpack-plugin');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
     'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
     './source/app.jsx'
   ],
